36fc1aa3cbddb2193806fd0b5bbd8056bba36667
Evan Marcey <evanmarcey@gmail.com>
2022-02-24 12:00:05 -0500

    Adding url route

--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 
 Little utility to widen square images with a white background to appease our Notion docs
 
-To use:
+## Run with local image
 
 1. Put your original image in the `originals` folder
-2. Run `python main.py --original_file={name of your file, without directory}`, e.g. `python main.py --original_file=guang.jpg`
-3. Output will be placed in `modified` folder
+1. Run `python main.py --original_file={name of your file, without directory}`, e.g. `python main.py --original_file=guang.jpg`
+1. Output will be placed in `modified` folder
+
+
+## Run from url
+
+1. Run `python main.py --original_url={name of your file, without directory}`, e.g. `python main.py --original_url="https://media-exp1.licdn.com/dms/image/C4D03AQE5cgP4rzpHeQ/profile-displayphoto-shrink_200_200/0/1628015750830?e=1651104000&v=beta&t=aQ51hfnG_i7wca_wePFHiubG1o0Wxi1HQHdI0Vg_iWo"`
+1. Output will be placed in `modified` folder
--- main.py
+++ main.py
@@ -1,30 +1,55 @@
 import argparse
+from datetime import datetime
 import os
 from PIL import Image
+import requests
+from typing import Optional, Tuple
 
 
 SOURCE_DIR = f"{os.getcwd()}/originals"
 OUTPUT_DIR = f"{os.getcwd()}/modified"
 
 parser = argparse.ArgumentParser(description="help")
-parser.add_argument("--original_file", type=str, help="name of the original file to use")
+parser.add_argument("--original_file", type=str, help="name of the original file to use", default=None)
+parser.add_argument("--original_url", type=str, help="url for the original file to use", default=None)
 parser.add_argument("--source_dir", type=str, help="name of the source directory", default=SOURCE_DIR)
 parser.add_argument("--output_dir", type=str, help="name of the output directory", default=OUTPUT_DIR)
 
 
-def main(original_file: str, source_dir: str, output_dir: str) -> None:
+def transform_and_save(original: Image.Image, output_dir: str, filename: str, file_extension: str) -> None:
+    background = Image.new("RGB", (original.width * 2, original.height), "white")
+    background.paste(original, (original.width // 2, 0))
+    background.save(f"{output_dir.rstrip('/')}/{filename}_modified.{file_extension}")
+
+
+def from_image(original_file: str, source_dir: str) -> Tuple[Image.Image, str, str]:
     original = Image.open(f"{source_dir.rstrip('/')}/{original_file}")
     if original.height != original.width:
         raise ValueError("Program only handles square files")
-
-    background = Image.new("RGB", (original.width * 2, original.height), "white")
-    background.paste(original, (original.width // 2, 0))
     full_filename = original_file.split("/")[-1].split(".")
     filename = full_filename[0]
     extension = ".".join(full_filename[1:])
-    background.save(f"{output_dir.rstrip('/')}/{filename}_modified.{extension}")
+    return original, filename, extension
+
+
+def from_url(original_url: str) -> Tuple[Image.Image, str, str]:
+    image = Image.open(requests.get(original_url, stream=True).raw)
+    return image, f"url_image_{datetime.now().strftime('%Y%m%d_%H%M%S')}", "jpg"
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    main(args.original_file, args.source_dir, args.output_dir)
+    original_image: Optional[Image.Image] = None
+    filename = ""
+    file_extension = ""
+    if args.original_file:
+        original_image, filename, file_extension = from_image(args.original_file, args.source_dir)
+    elif args.original_url:
+        original_image, filename, file_extension = from_url(args.original_url)
+    else:
+        raise ValueError("Need either original file or url")
+
+    if not original_image:
+        raise ValueError("No image created")
+
+    transform_and_save(original_image, args.output_dir, filename, file_extension)
